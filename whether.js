82c8304ca5d3efdaa5e941f408faf9eb49110334
James Kolsby <jrkolsby@mac.com>
2015-11-15 14:17:56 -0500

    Begun constructor

--- whether.js
+++ whether.js
@@ -1,6 +1,19 @@
 var weather = {
 
 }
-$(function(){
 
+function UserInterface() {
+	// Private fields
+	var top = $('.option#top');
+	var bottom = $('.option#bottom');
+
+	// Public fields
+	this.removeTop = function() {
+		top.remove();
+	}
+}
+
+$(function(){
+	var userInterface = new UserInterface();
+	userInterface.removeTop();
 });
