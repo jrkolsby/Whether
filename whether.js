7d7dad0360ba67a6cbc1f9b1f23c9a5d9a3fae95
James Kolsby <jrkolsby@mac.com>
2015-11-15 16:48:08 -0500

    Added OOP-related comments

--- whether.js
+++ whether.js
@@ -3,17 +3,35 @@ var weather = {
 }
 
 function UserInterface() {
-	// Private fields
-	var top = $('.option#top');
-	var bottom = $('.option#bottom');
 
 	// Public fields
+	this.TEMP_TAG = "h1";
+	this.DESC_TAG = "h2";
+
+	// Private fields
+	var top = $('.option#top'),
+		bottom = $('.option#bottom'),
+
+		topTemp = top.children(this.TEMP_TAG),
+		topDesc = top.children(this.DESC_TAG),
+
+		bottomTemp = bottom.children(this.TEMP_TAG),
+		bottomDesc = bottom.children(this.DESC_TAG)
+
+	// Public methods
 	this.removeTop = function() {
+		top.remove();
+	}
+
+	// Private methods
+	var removeTop = function() {
 		top.remove();
 	}
 }
 
 $(function(){
+	// Main loop
 	var userInterface = new UserInterface();
 	userInterface.removeTop();
+	console.log(userInterface.TEMP_TAG);
 });
