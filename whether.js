e100fbce933cab967e3e372cba9760f22b7c97e6
James Kolsby <jrkolsby@mac.com>
2015-11-17 01:32:42 -0500

    Added relevant DOM object data structure

--- whether.js
+++ whether.js
@@ -1,9 +1,29 @@
 var UserInterface = function() {
+
+	var SCROLL_TEMP_RATIO = 20;
+
+	var element = {
+		city: $("#sidebar h1"),
+		country: $("#sidebar h4"),
+		temp: $("#forcast .temp"),
+		desc: $("#forcast .desc"),
+		icon: $("#forcast .icon")
+	},
+		scrollIncrement = 0;
+
+	this.setTemperature = function(t) { element.temp.text(t) }
+	this.setCity = function(c) { element.city.text(c) }
+	this.setCountry = function(c) { element.country.text(c) }
+
 	$(window).on('mousewheel', function(event, delta) {
-		console.log(event.originalEvent.deltaY);
+		scrollIncrement += event.originalEvent.deltaY;
+		if (scrollIncrement >= SCROLL_TEMP_RATIO) {
+
+		}
 	});
 }
 
-$(function(){
+$(document).ready(function() {
 	var userInterface = new UserInterface();
+	userInterface.setTemperature(100);
 });
