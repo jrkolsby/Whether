438e2f86fc6da68ea6d039ba29dede104315dc00
James Kolsby <jrkolsby@mac.com>
2015-11-16 20:14:50 -0500

    Changed interface direction, initial commit

--- whether.js
+++ whether.js
@@ -1,43 +1,7 @@
-var weather = {
-
-}
-
 var UserInterface = function() {
-	this.TEMP_TAG = "h1";
-	this.DESC_TAG = "h2";
-	this.OPTION_ID = ".option";
 
-	var options = [];
-
-	for (var i = 0; i < $(this.OPTION_ID).length; i++) {
-		var currentOption = $(this.OPTION_ID + ":eq(" + i + ")");
-		options[i] = {
-			outer: currentOption,
-			temp: currentOption.children(this.TEMP_TAG),
-			desc: currentOption.children(this.DESC_TAG)
-		}
-	}
-
-	this.setForcast = function(index, temp, desc) {
-		options[index].temp.text(temp);
-		options[index].desc.text(desc);
-	}
-
-	/* Sets the option's background given a
-	 * hex code
-	 */
-	this.setBackground = function(index, hex) {
-		if (hex.charAt(0) !== "#") {
-			hex = "#" + hex;
-		}
-		options[index].outer.css({
-			background: hex
-		});
-	}
 }
 
 $(function(){
 	var userInterface = new UserInterface();
-	userInterface.setForcast(0, 69, "eyyy");
-	userInterface.setBackground(0, "#00ff00");
 });
