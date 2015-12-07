1d09f747e721b878b2af57752dbd32936cac1655
James Kolsby <jrkolsby@mac.com>
2015-12-07 17:04:36 -0500

    Finished implementation of MakeRoundAction

--- whether.js
+++ whether.js
@@ -10,20 +10,10 @@ var Weather = function(){
 	this.getCityCoord = function() { return cityCoord }
 	this.getWeatherState = function() { return weatherState }
 
-	var updateCityData = function(data) {
-		cityName = data.name;
-		cityCoord = {lat: parseFloat(data.coord.lat), lng: parseFloat(data.coord.lon)};
-
-		temp = data.main.temp;
-
-		var stateID = data.weather[0].id;
-		weatherState = weatherStateList[stateID];
-	}
-
 	//set a new random city
-	this.setCity = function(cityID, complete){
+	this.setCity = function(cityID, complete) {
 
-		var self = this; // Preserve Context
+		var self = this; // Preserve context outside AJAX
 
 		//call API and store to fields
 		$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=" + cityID + "&APPID=" + keys.weather, function(data){
@@ -33,31 +23,48 @@ var Weather = function(){
 			complete.call(self);
 		});
 	}
+
+	var updateCityData = function(data) {
+
+		cityName = data.name;
+
+		cityCoord = { lat: parseFloat(data.coord.lat),
+					  lng: parseFloat(data.coord.lon) };
+
+		temp = data.main.temp;
+
+		var stateID = data.weather[0].id;
+		weatherState = weatherStateList[stateID];
+
+	}
 }
 
 var Map = function(mapID) {
 
 	var myOptions = {
+
 		 zoom: 5,
-		 center: {lat: 0, lng: 0},
+		 center: { lat: 0, lng: 0 },
+
 		 mapTypeId: google.maps.MapTypeId.ROADMAP,
 		 disableDefaultUI: true,
 	};
 
 	//set the coordinate for the map
-	this.setCoord = function(aLatLng){
+	this.setCoord = function(aLatLng) {
 		myOptions.center = aLatLng;
 		updateMap();
 	}
 
 	//set the zoom level for the map
-	this.setZoom = function(aZoom){
+	this.setZoom = function(aZoom) {
 		myOptions.zoom = aZoom;
 		updateMap();
 	}
 
-	var updateMap = function(){
+	var updateMap = function() {
 		var map = new google.maps.Map(document.getElementById(mapID), myOptions);
+
 		var marker = new google.maps.Marker({
 			position: myOptions.center,
 			map: map,
@@ -79,12 +86,12 @@ var UserInterface = function() {
 		DRAG_DESC_RATIO = 50;
 
 	var element = {
-		sidebar: $("#sidebar"),
-		city: $("#sidebar h1"),
-		country: $("#sidebar h4"),
-		temp: $("#forcast .temp"),
+		sidebar: 	   $("#sidebar"),
+		city: 		   $("#sidebar h1"),
+		country: 	   $("#sidebar h4"),
+		temp: 		   $("#forcast .temp"),
 		weatherStates: $("#forcast .weatherStates"),
-		icon: $("#forcast .icon")
+		icon: 		   $("#forcast .icon")
 	}
 
 	var scrollIncrementY = 0,
@@ -97,21 +104,26 @@ var UserInterface = function() {
 
 	this.setWeatherState = function(a) {
 		weatherStates = a;
-		// Create elements for each description
+		// TODO: Create elements for each description
 	}
+
 	this.setTemperature = function(t) {
 		temp = t;
 		element.temp.text(t);
 	}
+
 	this.setWeatherStateIndex = function(a) {
 		weatherStateIndex = a;
 		element.weatherStates.text(weatherStates[weatherStateIndex]);
 	}
 
+
 	var changeTemperature = function(dt) {
 		temp += dt;
 		element.temp.text(temp)
 	}
+
+	// TODO: Slide between DOM elements for each State
 
 	var nextWeatherState = function() {
 		if (weatherStateIndex+1 < weatherStates.length) { weatherStateIndex += 1 }
@@ -205,9 +217,7 @@ var UserInterface = function() {
 
 var MakeRoundAction = function(game, userInterface, map, weather) {
 
-	var WEATHER_STATE_OPTIONS = 3;
-
-	var tempLocale;
+	var WEATHER_STATE_OPTIONS = 5;
 
 	var city = "",
 		coord = {},
@@ -215,10 +225,27 @@ var MakeRoundAction = function(game, userInterface, map, weather) {
 		temp = 0,
 		weatherStates = [],
 		correctStateIndex = 0;
-
-	this.setTempLocale = function(i) { tempLocale = i }
+	
 	this.FARENHEIT = 0;
 	this.CELCIUS = 1;
+
+	this.setTempLocale = function(i) { tempLocale = i }
+
+	this.execute = function(complete) {
+
+		// Preserve context
+		var self = this;
+
+		weather.setCity(getRandomCityID(), function() {
+
+			makeStates.call(self);
+
+			updateInterface.call(self);
+			updateMap.call(self);
+
+			complete(temp, correctStateIndex);
+		});
+	}
 
 	var getRandomCityID = function() {
 
@@ -282,25 +309,10 @@ var MakeRoundAction = function(game, userInterface, map, weather) {
 	}
 
 	var updateMap = function() { map.setCoord(coord) }
-
-	this.execute = function(complete) {
-
-		var self = this; // Preserve Context
-
-		weather.setCity(getRandomCityID(), function() {
-
-			makeStates();
-
-			updateInterface();
-			updateMap();
-
-			complete();
-		});
-	}
 }
 
 var ScoreRoundAction = function(game, interface) {
-
+	// TODO: Make sure Celcius and Farenheit are scored differently
 }
 
 var ScoreGameAction = function(game) {
@@ -308,6 +320,11 @@ var ScoreGameAction = function(game) {
 }
 
 var Game = function() {
+
+	var round = {
+		temp: 0,
+		stateIndex: 0
+	}
 
 	var userInterface = new UserInterface();
 	var map = new Map("map");
@@ -318,7 +335,8 @@ var Game = function() {
 	makeRound.setTempLocale(makeRound.FARENHEIT);
 
 	makeRound.execute(function(correctTemp, correctStateIndex) {
-
+		round.temp = correctTemp;
+		round.stateIndex = correctStateIndex;
 	});
 }
 
